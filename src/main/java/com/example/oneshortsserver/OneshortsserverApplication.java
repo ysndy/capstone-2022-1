2243bb29b4c77a95abb9526e38e5a0ebc3f30719
ysndy <ysndy@naver.com>
2022-11-09 15:42:30 +0900

    init commit

---
+++ src/main/java/com/example/oneshortsserver/OneshortsserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneshortsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneshortsserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneshortsserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oneshortsserver/OneshortsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneshortsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneshortsserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
