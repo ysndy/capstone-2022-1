1dcc660f20448774ae83f6fef989f59c932fc765
ysndy <ysndy@naver.com>
2022-11-23 14:46:44 +0900

    vue 연동

---
+++ src/main/java/com/example/oneshortsserver/dto/UploadDTO.java
@@ -0,0 +1,16 @@
+package com.example.oneshortsserver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UploadDTO {
+
+
+    String id;
+    String title;
+    String detail;
+
+
+}
